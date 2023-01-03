6b55cda6a45f3db150e0832e3dc4b7b149c3fd7e
Serhii Bakun <gizzmo599@gmail.com>
2023-01-03 21:50:30 +0200

    refactored to hooks

---
+++ src/components/App/App.js
@@ -0,0 +1,60 @@
+import { useState, useEffect } from 'react';
+import { nanoid } from 'nanoid';
+import { Container } from './App.styled';
+import { ContactForm } from '../ContactForm/ContactForm';
+import { Filter } from '../Filter/Filter';
+import { ContactList } from 'components/ContactList/ContactList';
+
+export const App = () => {
+  const [contacts, setContacts] = useState(
+    JSON.parse(window.localStorage.getItem('contacts')) ?? []
+  );
+  const [filter, setFilter] = useState('');
+
+  useEffect(() => {
+    localStorage.setItem('contacts', JSON.stringify(contacts));
+  }, [contacts]);
+
+  const handleChange = e => {
+    switch (e.target.name) {
+      case 'filter':
+        setFilter(e.target.value);
+        break;
+      default:
+        return;
+    }
+  };
+
+  const handleSubmit = (name, number) => {
+    if (contacts.find(contact => contact.name === name)) {
+      return window.alert(`${name} is already in contacts`);
+    }
+    setContacts(prevState => [...prevState, { id: nanoid(), name, number }]);
+  };
+
+  const deleteContact = contactId => {
+    setContacts(prevState =>
+      prevState.filter(contact => contact.id !== contactId)
+    );
+  };
+
+  const normalizedFilter = filter.toLowerCase();
+  const filteredContacts =
+    contacts.length > 0
+      ? contacts.filter(contact =>
+          contact.name.toLowerCase().includes(normalizedFilter)
+        )
+      : [];
+
+  return (
+    <Container>
+      <h2>Phonebook</h2>
+      <ContactForm onSubmit={handleSubmit} />
+      <h2>Contacts</h2>
+      <Filter value={filter} onChange={handleChange} />
+      {filteredContacts.length > 0 && (
+        <ContactList contacts={filteredContacts} onDelete={deleteContact} />
+      )}
+    </Container>
+  );
+};
